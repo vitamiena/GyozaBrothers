fb2165231416e7f7afa51d6c99b061a61a23a4a1
vitamiena <s13t210@stmail.eng.kagawa-u.ac.jp>
2015-11-12 20:42:49 +0900

    Fix indent miss

--- src/Material/Player.java
+++ src/Material/Player.java
@@ -9,45 +9,45 @@ import Env.*;
 public class Player extends AbstractCharacter {
   private KeyController keyController = KeyController.getInstance();
     
-    // コンストラクタ
-    public Player(int w, int h, int x, int y) {
-      super(w, h, x, y);
-    }
-  
-    // 描画
-    public void draw(Graphics g) {
-      g.drawRect(getX(), getY(), getWidth(), getHeight());
-    }
-  
-    public void draw(Graphics g, int tx) {
-      g.drawRect(tx, getY(), getWidth(), getHeight());
-    }
-  
-    public void move() {
-      // プレイヤの移動(キー入力)
-      Vector v = getMoveDir();
-        
-      // ジャンプキーが押されたとき
-      if ( keyController.getUp() == KeyController.Key.Press ) {
-        v.vertical -= 10;
-        keyController.setUpKeep();
-      } 
-
-      // 左右キーが押されたとき
-      if ( keyController.getLeft() == KeyController.Key.Press ) {
-        keyController.setLeftKeep();
-      } else if ( keyController.getRight() == KeyController.Key.Press ) {
-        keyController.setRightKeep();
-      }     
+  // コンストラクタ
+  public Player(int w, int h, int x, int y) {
+    super(w, h, x, y);
+  }
+ 
+  // 描画
+  public void draw(Graphics g) {
+    g.drawRect(getX(), getY(), getWidth(), getHeight());
+  }
+ 
+  public void draw(Graphics g, int tx) {
+    g.drawRect(tx, getY(), getWidth(), getHeight());
+  }
+ 
+  public void move() {
+    // プレイヤの移動(キー入力)
+    Vector v = getMoveDir();
       
-      // 左右への移動方向の設定
-      if ( keyController.getLeft() == KeyController.Key.Keep ) {
-        v.horizontal = -5;
-      } else if ( keyController.getRight() == KeyController.Key.Keep ) {
-        v.horizontal = 5;
-      } else {
-        v.horizontal = 0;
-      }
+    // ジャンプキーが押されたとき
+    if ( keyController.getUp() == KeyController.Key.Press ) {
+      v.vertical -= 10;
+      keyController.setUpKeep();
+    } 
+
+    // 左右キーが押されたとき
+    if ( keyController.getLeft() == KeyController.Key.Press ) {
+      keyController.setLeftKeep();
+    } else if ( keyController.getRight() == KeyController.Key.Press ) {
+      keyController.setRightKeep();
+    }     
+    
+    // 左右への移動方向の設定
+    if ( keyController.getLeft() == KeyController.Key.Keep ) {
+      v.horizontal = -5;
+    } else if ( keyController.getRight() == KeyController.Key.Keep ) {
+      v.horizontal = 5;
+    } else {
+      v.horizontal = 0;
+    }
     setMoveDir(v);
   }
   
